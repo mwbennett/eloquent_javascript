df11958bec004984f0c54f6dea598373695e6094
Mark Bennett <mwbennett@wesleyan.edu>
2014-09-29 10:59:30 -0400

    first commit

---
+++ Chapter02/looping_a_triangle.js
@@ -0,0 +1,18 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle: 
+
+#
+##
+###
+####
+#####
+######
+#######
+*/ 
+
+var str = "";
+for (var count = 0; count < 7; count++){
+  str = str + "#"
+  console.log(str)
+}
+
